6a0b48d924b8283d325bb9f3fb9b89313ebe0204
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-21 13:59:55 +0200

    Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        Menu
-    </div>
+    <nav>
+        <div class="d-flex justify-content-between align-items-center container">
+            <a href="#">
+                <img src="../assets/images/dark-logo.png" alt="Logo MaxCoach">
+            </a>
+            <ul class="d-flex align-items-center">
+                <li v-for="n in 6" :key="n">
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+            <ul class="d-flex align-items-center">
+                <li v-for="n in 4" :key="n">
+                    <a href="#">Social</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -13,5 +27,25 @@ export default {
 <style lang="scss" scoped>
     @import '../assets/style/variables.scss';
     @import '../assets/style/mixins.scss';
+
+    nav {
+        height: 90px;
+
+        div {
+            height: 100%;
+
+            a {
+                img {
+                    width: 150px;
+                }
+            }
+
+            ul {
+                li {
+                    margin: 0 10px;
+                }
+            }
+        }
+    }
 
 </style>
