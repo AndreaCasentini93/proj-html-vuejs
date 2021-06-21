316c15a4071c3fe694316f695d24ec2918c50773
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-21 17:25:06 +0200

    Jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,14 +1,23 @@
 <template>
-    <div id="jumbotron" class="position-relative">
-        <img class="artist-shape-02" src="../assets/images/artist-shape-02.png" alt="artist-shape-02">
-        <img class="artist-shape-01" src="../assets/images/artist-shape-01.png" alt="artist-shape-01">
-        <img class="artist-hero-image-04" src="../assets/images/artist-hero-image-04.jpg" alt="artist-hero-image-04">
-        <img class="artist-hero-image-03" src="../assets/images/artist-hero-image-03.png" alt="artist-hero-image-03">
-        <img class="artist-shape-03" src="../assets/images/artist-shape-03.png" alt="artist-shape-03">
-        <img class="artist-hero-image-01" src="../assets/images/artist-hero-image-01.jpg" alt="artist-hero-image-01">
-        <img class="artist-shape-04" src="../assets/images/artist-shape-04.png" alt="artist-shape-04">
-        <!-- <img class="artist-shape-05" src="../assets/images/artist-shape-05.png" alt="artist-shape-05">
-        <img class="artist-hero-image-02" src="../assets/images/artist-hero-image-02.jpg" alt="artist-hero-image-02"> -->
+    <div id="jumbotron" class="position-relative d-flex justify-content-center align-items-center">
+        <!-- IMAGES -->
+        <img class="form_1" src="../assets/images/artist-shape-02.png" alt="form_1">
+        <img class="form_2" src="../assets/images/artist-shape-01.png" alt="form_2">
+        <img class="feather" src="../assets/images/artist-shape-04.png" alt="feather">
+        <img class="colors_1" src="../assets/images/artist-shape-05.png" alt="colors_1">
+        <img class="photo" src="../assets/images/artist-hero-image-04.jpg" alt="photo">
+        <img class="profile" src="../assets/images/artist-hero-image-03.png" alt="profile">
+        <img class="points" src="../assets/images/artist-shape-03.png" alt="points">
+        <img class="colors_2" src="../assets/images/artist-hero-image-02.jpg" alt="colors_2">
+        <img class="monkey" src="../assets/images/artist-hero-image-01.jpg" alt="monkey">
+        <!-- /IMAGES -->
+
+        <!-- TEXT -->
+        <div class="text-center">
+            <h2>Hello, Im Matin</h2>
+            <h3>Artist Coaching And Mentoring<br>Might Be For You</h3>
+        </div>
+        <!-- /TEXT -->
     </div>
 </template>
 
@@ -31,39 +40,73 @@ export default {
             position: absolute;
         }
 
-        .artist-shape-02 {
+        .form_1 {
+            top: 0;
             left: 0;
             width: 700px;
         }
         
-        .artist-shape-01 {
+        .form_2 {
             right: 10%;
-            width: 580px;
+            width: 540px;
         }
 
-        .artist-hero-image-04 {
-            bottom: 8%;
+        .photo {
             right: 15%;
+            bottom: 9%;
             width: 260px;
+            box-shadow: 0 0 25px 1px lighten($base-color, 30%);
         }
 
-        .artist-hero-image-03 {
+        .profile {
             bottom: 2%;
             left: 26%;
             width: 200px;
         }
 
-        .artist-hero-image-01 {
-            bottom: 8%;
+        .monkey {
+            bottom: 11%;
             left: 8%;
             width: 150px;
-            box-shadow: 0 0 25px 1px lighten($base-color, 20%);
+            box-shadow: 0 0 25px 1px lighten($base-color, 30%);
         }
 
-        .artist-shape-03 {
-            left: 0;
+        .points {
             bottom: 10%;
+            left: 0;
             width: 282px;
+        }
+
+        .feather {
+            right: 11%;
+            bottom: 48%;
+            width: 100px;
+        }
+
+        .colors_1 {
+            right: 7%;
+            bottom: 0;
+            width: 270px;
+        }
+
+        .colors_2 {
+            bottom: 37%;
+            left: 17%;
+            width: 240px;
+            box-shadow: 0 0 25px 1px lighten($base-color, 30%);
+        }
+
+        div {
+            h2 {
+                @include section-title;
+            }
+
+            h3 {
+                line-height: 1.5;
+                font-size: 31px;
+                font-weight: 700;
+                color: $sub-title-color;
+            }
         }
     }
 </style>
