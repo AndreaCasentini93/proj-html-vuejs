5804acc60014d5a428e8922dfdd13897bf32abae
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-22 13:12:14 +0200

    Create Like Column Right

--- src/components/CreateLike.vue
+++ src/components/CreateLike.vue
@@ -1,6 +1,30 @@
 <template>
     <section>
-        CREATE LIKE
+        <div class="container">
+            <div class="row">
+                <div class="col-10 offset-1">
+                    <div class="row">
+                        <!-- COLUMN LEFT -->
+                        <div class="col-12 col-md-6">
+                            ciao
+                        </div>
+                        <!-- /COLUMN LEFT -->
+
+                        <!-- COLUMN RIGHT -->
+                        <div class="col-12 col-md-6">
+                            <p>
+                                As a coach and menthor, my ultimate goal is to motivate visual artists toward inspirated action. I help you get you back on track to being the productive, creative, meaningful and authentic artist you truly want to be. I support artists in crafting and living a creative life as well as feeling good about themselves and their art.
+                                <br>
+                                <br>
+                                As an artist and an educator for early 30 years, I understand what it takes to create.
+                            </p>
+                            <a href="#" class="btn_type_3">Get started today</a>
+                        </div>
+                        <!-- /COLUMN RIGHT -->
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -14,4 +38,8 @@ export default {
     @import '../assets/style/variables.scss';
     @import '../assets/style/mixins.scss';
 
+    p {
+        margin-bottom: 50px;
+        color: $base-color;
+    }
 </style>
