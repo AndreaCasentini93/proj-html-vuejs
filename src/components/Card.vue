dee9dfc3514983f2aaa86af03b74d5fdf16e027b
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-22 22:25:08 +0200

    Courses and Cards Complete

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="single_card">
-        Card
+        <img :src="card.image" :alt="card.title">
+        <h5>&#xFF04;{{ card.subTitle }}</h5>
+        <h4>{{ card.title }}</h4>
+        <div class="d-flex align-items-center icon_box">
+            <i :class="card.classIcon1"></i><span>{{ card.textIcon1 }}</span>
+            <i :class="card.classIcon2"></i><span>{{ card.textIcon2 }}</span>
+        </div>
     </div>
 </template>
 
@@ -21,7 +27,41 @@ export default {
     .courses_cards {
 
         .single_card {
-            color: red;
+            padding: 35px 20px;
+
+            img {
+                max-width: 100%;
+                margin-bottom: 20px;
+            }
+
+            h5 {
+                font-size: 15px;
+                font-weight: 700;
+                color: $ashtag-color;
+            }
+
+            h4 {
+                width: 70%;
+                margin-bottom: 20px;
+                font-size: 16px;
+                font-weight: 700;
+                color: $sub-title-color;
+            }
+
+            .icon_box {
+                font-size: 12px;
+                vertical-align: middle;
+                color: $base-color;
+
+                i {
+                    margin-right: 5px;
+                    font-size: 13px;
+                }
+
+                span {
+                    margin-right: 10px;
+                }
+            }
         }
 
     }
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -16,12 +16,12 @@
 
             <!-- CARDS -->
             <div class="row courses_cards">
-                <Card v-for="card,index in cards" :key="index" class="col-12 col-md-6 col-lg-3" :card="sindleCard"/>
+                <Card v-for="singleCard,index in cards" :key="index" class="col-12 col-md-6 col-lg-3" :card="singleCard"/>
             </div>
             <!-- /CARDS -->
 
             <!-- BUTTON -->
-            <div class="text-center">
+            <div class="text-center button_box">
                 <a href="#" class="btn_type_3">View all courses <i class="fas fa-long-arrow-alt-right"></i></a>
             </div>
             <!-- /BUTTON -->
@@ -43,58 +43,74 @@ export default {
                 {
                     title: 'The Acrylic Painting Accademy',
                     image: require('../assets/images/artist-course-08-480x480.jpg'),
-                    price: '&#x24;18.00',
+                    subTitle: '18.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '4 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'Drawing and Shading: Complete Course',
                     image: require('../assets/images/artist-course-07-480x480.jpg'),
-                    price: '&#x24;21.00',
+                    subTitle: '21.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '14 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'The Color Theory for Digital Artist',
                     image: require('../assets/images/artist-course-06-480x480.jpg'),
-                    price: '&#x24;19.00',
+                    subTitle: '19.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '7 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'Ultimate Guide to Digital Sketching for Beginner',
                     image: require('../assets/images/artist-course-05-480x480.jpg'),
-                    price: '&#x24;35.00',
+                    subTitle: '35.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '14 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'Portrait Drawing The Smart Way',
                     image: require('../assets/images/artist-course-04-480x480.jpg'),
-                    price: '&#x24;19.00',
+                    subTitle: '19.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '2 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'Mastering Watercolor Painting from Beginner',
                     image: require('../assets/images/artist-course-03-480x480.jpg'),
-                    price: '&#x24;19.00',
+                    subTitle: '19.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '9 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'The Artist & Science of Drawing',
                     image: require('../assets/images/artist-course-02-480x480.jpg'),
-                    price: '&#x24;25.00',
+                    subTitle: '25.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '4 Lessons',
+                    textIcon2: '50 Students'
                 },
                 {
                     title: 'The Colored Pencil Drawing Course',
                     image: require('../assets/images/artist-course-01-480x480.jpg'),
-                    price: '&#x24;22.00',
+                    subTitle: '22.00',
                     classIcon1: 'far fa-file-alt',
                     classIcon2: 'fas fa-gamepad',
+                    textIcon1: '6 Lessons',
+                    textIcon2: '50 Students'
                 }
             ]
         }
@@ -113,21 +129,23 @@ export default {
         & > img {
             position: absolute;
             width: 400px;
+            z-index: -1;
         }
 
         .text_decoration {
             position: absolute;
-            top: 55%;
-            right: -8%;
+            top: 45%;
+            right: -9.5%;
             text-transform: uppercase;
-            font-size: 100px;
+            font-size: 120px;
             font-family: $decoration-font;
-            color: lighten($ashtag-color, 30%);
+            color: lighten($ashtag-color, 35%);
             transform: rotate(90deg);
+            z-index: -1;
         }
 
         .form_2_1 {
-            bottom: 0;
+            bottom: 10%;
             left: -15%;
             filter: invert(100%) brightness(150%);
             transform: rotate(10deg);
@@ -153,8 +171,11 @@ export default {
             }
         }
 
-        div {
+        .button_box {
+            margin-top: 30px;
+
             .btn_type_3 {
+
                 i {
                     vertical-align: middle;
                 }
@@ -168,5 +189,24 @@ export default {
             display: none !important;
         }
 
+        h2 {
+            font-size: 30px !important;
+        }
+
+        h3 {
+            font-size: 20px !important;
+        }
+
+        h4 {
+            font-size: 13px !important;
+        }
+
+        h5 {
+            font-size: 12px !important;
+        }
+
+        p {
+            font-size: 11px !important;
+        }
     }
 </style>
