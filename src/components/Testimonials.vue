9661a7d516cf6a1b3fdad1aa538e8f4018f33ce6
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-22 16:01:58 +0200

    Slider

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,6 +1,19 @@
 <template>
     <section>
-        Testimonials
+        <div class="container">
+            <!-- TITLES -->
+            <div class="text-center">
+                <h2>Testimonials</h2>
+                <h3>Why do people love me?</h3>
+            </div>
+            <!-- /TITLES -->
+
+            <!-- SLIDER -->
+            <div class="slider">
+                
+            </div>
+            <!-- SLIDER -->
+        </div>
     </section>
 </template>
 
@@ -14,4 +27,23 @@ export default {
     @import '../assets/style/variables.scss';
     @import '../assets/style/mixins.scss';
 
+    section {
+        padding: 30px 0;
+        background-color: $bg-testimonials;
+
+        div {
+
+            h2 {
+                @include section-title-35px;
+            }
+
+            h3 {
+                line-height: 1.2;
+                margin-bottom: 50px;
+                font-size: 25px;
+                font-weight: 700;
+                color: $sub-title-color;
+            }
+        }
+    }
 </style>
